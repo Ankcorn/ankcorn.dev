2090019e2270e8d51b2cbe3be5b35727cac97590
ThomasAnkcorn <thomasankcorn@gmail.com>
2019-06-25 22:01:49 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  title: 'Blog by Thomas Ankcorn',
+  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+  ],
+  author: {
+    name: 'Thomas Ankcorn',
+    photo: '/photo.jpg',
+    bio: 'A Personal Blog, I write about technology to remind me when I forget things. If you find it useful it was probably an accident.',
+    contacts: {
+      email: 'thomas@ankcorn.dev',
+      telegram: 'https://t.me/thomasankcorn',
+      twitter: 'https://twitter.com/thomasankcorn',
+      github: 'https://github.com/ankcorn',
+    }
+  }
+};
