235019cd6932951d35a19d77dc1cac847523b58e
Thomas Ankcorn <tankcorn@cloudflare.com>
2026-01-15 22:48:27 +0000

    add viewport meta

--- src/layout.tsx
+++ src/layout.tsx
@@ -4,6 +4,7 @@ export const Layout: FC<{ showFooter?: boolean; children: Child }> = (props) => 
   return (
     <html>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap" rel="stylesheet" />
